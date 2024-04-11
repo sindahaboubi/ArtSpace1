6399d4bd05b7d91af681e78981eda6ddb3b4cbd7
soulaymabendahsen <souleima.bendahsen@gmail.com>
2024-04-11 23:51:55 +0200

    Version 1

---
+++ src/main/java/Controllers/General/GeneralLayouts.java
@@ -0,0 +1,45 @@
+package Controllers.General;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GeneralLayouts {
+    @FXML
+    private Label accueilLabel;
+
+    @FXML
+    private Label AProposLabel;
+
+    @FXML
+    private void retournerALaPagePrecedenteAdmint() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/OeuvreArtistique/General/accueil.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) accueilLabel.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void retournerALaPageAPropos() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/OeuvreArtistique/General/propos.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) AProposLabel.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
